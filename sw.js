f7f66adcbff79a8b6d953032a3a36b38d5dcd8b5
williamjones2022-png <williamjones2022@icloud.com>
2026-05-20 20:32:01 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE = 'wills-budget-v4';
-const FILES = ['./','./index.html','./manifest.json','./icon.svg'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
